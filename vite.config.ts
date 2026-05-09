7ecb5406468a8cef91c29a94615680cc79713af8
MFAlex <132637207+MFAlex@users.noreply.github.com>
2026-05-09 23:55:47 +1200

    Fix build for updated JASSUB worker

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ["metube.thisisalex.net"]
   },
+  worker: {
+    format: 'es'
+  }
 })
